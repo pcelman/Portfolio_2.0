00cb165352f25b007dfc5d472395a372bfe62396
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2023-04-24 18:56:42 -0300

    refactor: about orientation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,11 +11,12 @@ import { RiReactjsFill } from "react-icons/ri";
 import {
   SiTailwindcss,
   SiBootstrap,
-//   SiMaterialui,
+  //   SiMaterialui,
   SiExpress,
   SiSequelize,
 } from "react-icons/si";
-import {RiFilePaperLine} from "react-icons/ri"
+
+import { RiFilePaperLine } from "react-icons/ri";
 import { GrNode } from "react-icons/gr";
 import cv from "../assets/paulacelman.pdf";
 import "../styles/About.css";
@@ -24,25 +25,30 @@ const About = () => {
   return (
     <div className="about" id="home">
       <div className="container">
-        <section className="paralax__container">
-
-        {/* <div className="about__image"></div> */}
         <section className="about__paralax">
-        
-          <img src={g} alt="broken edges circle" width="350px" className="about__bg-back image"/>
 
-          <img src={b} alt="broken edges circle" width="350px" className="about__bg-middle image"/>
-      
-      
-         <img
-          className="about__foreground image"
-          src={paula}
-          alt="Smiling woman"
-          width="350px"
+          <img
+            src={g}
+            alt="broken edges circle"
+            width="350px"
+            className="about__bg-back image"
           />
-     
-        </section>
-        {/* <img
+
+          <img
+            src={b}
+            alt="broken edges circle"
+            width="350px"
+            className="about__bg-middle image"
+          />
+
+          <img
+            className="about__foreground image"
+            src={paula}
+            alt="Smiling woman"
+            width="350px"
+          />
+
+          {/* <img
           className="about__img-md"
           src={paulamd}
           alt="Smiling woman with a quote of Leonardo da Vinci on top that reads ´Simplicity is the ultimate sophistication´ "
@@ -62,7 +68,6 @@ const About = () => {
             with my dedication to producing high-quality work, sets me apart.
           </p>
           <p className="about__bold">
-            
             <a
               href={cv}
               target="_blank"
@@ -70,170 +75,158 @@ const About = () => {
               download="Paula_Celman_CV.pdf"
               className="download__cv"
             >
-               Download CV
+              Download CV
             </a>
             {/* <RiFilePaperLine /> */}
           </p>
+        </div>
+      </div>
+      {/* skills --------------- */}
+      <div className="about__skills">
+        <div className="about__skills__title">Skills</div>
+        <span className="line"></span>
+        <ul className="skills__content">
+          <li className="skills__item">
+            <div>JavaScript</div>
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <IoLogoJavascript
+                size={35}
+                className="skills__icon"
+                alt="Link to Javascript"
+              />
+            </a>
+          </li>
 
-          
-          <div className="skills">Skills</div>
-          <span className="line"></span>
-          <ul className="skills__content">
-            <li className="skills__item">
-              <div>JavaScript</div>
-              <a
-                href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-
-                <IoLogoJavascript
-                  size={35}
-                  className="skills__icon"
-                  alt="Link to Javascript"
-                />
-              </a>
-            </li>
-
-            <li className="skills__item">
-              <div>
-                React & <br /> React Native
-              </div>
-              <a
-                href="https://reactjs.org/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <RiReactjsFill
-                  size={35}
-                  className="skills__icon"
-                  alt="Link to React icon"
-                />
-              </a>
-            </li>
-            <li className="skills__item">
-              <div>Figma</div>
-              <a
-                href="https://www.figma.com/blog/figma-on-figma-how-we-built-figma-dot-coms-design-system/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <FiFigma
-                  size={35}
-                  className="skills__icon"
-                  alt="Link to Figma"
-                />
-              </a>
-            </li>
-            <li className="skills__item">
-              <div>CSS</div>
-              <a
-                href="https://www.w3schools.com/css/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <FaCss3Alt
-                  size={35}
-                  className="skills__icon"
-                  alt="Link to CSS"
-                />
-              </a>
-            </li>
-            <li className="skills__item">
-              <div>Tailwind</div>
-              <a
-                href="https://tailwindui.com/documentation"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <SiTailwindcss
-                  size={35}
-                  className="skills__icon"
-                  alt="Link to Tailwind"
-                />
-              </a>
-            </li>
-            <li className="skills__item">
-              <div>Bootstrap</div>
-              <a
-                href="https://alaskan-odyssey.vercel.app/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <SiBootstrap
-                  size={35}
-                  className="skills__icon"
-                  alt="Link to Bootstrap"
-                />
-              </a>
-            </li>
-            {/* <li className="skills__item">
-              <div>MaterialUI</div>
-              <a
-                href="https://mytodolist-six.vercel.app/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <SiMaterialui
-                  size={35}
-                  className="skills__icon"
-                  alt="Link to MaterialUI"
-                />
-              </a>
-            </li> */}
-            <li className="skills__item">
-              <div>NodeJs</div>
-              <a
-                href="https://nodejs.org/en/docs/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <GrNode size={35} className="skills__icon" alt="Link to Node" />
-              </a>
-            </li>
-            <li className="skills__item">
-              <div>Express</div>
-              <a
-                href="https://expressjs.com/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <SiExpress
-                  size={35}
-                  className="skills__icon"
-                  alt="Link to Express"
-                />
-              </a>
-            </li>
-            <li className="skills__item">
-              <div>Sequelize</div>
-              <a
-                href="https://sequelize.org/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <SiSequelize
-                  size={35}
-                  className="skills__icon"
-                  alt="Link to Sequelize"
-                />
-              </a>
-            </li>
-            <li className="skills__item">
-              <div>Git</div>
-              <a
-                href="https://git-scm.com/doc"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <FaGitAlt
-                  size={35}
-                  className="skills__icon"
-                  alt="Link to Git"
-                />
-              </a>
-            </li>
-            {/* <li className="skills__item">
+          <li className="skills__item">
+            <div>
+              React & <br /> React Native
+            </div>
+            <a
+              href="https://reactjs.org/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <RiReactjsFill
+                size={35}
+                className="skills__icon"
+                alt="Link to React icon"
+              />
+            </a>
+          </li>
+          <li className="skills__item">
+            <div>Figma</div>
+            <a
+              href="https://www.figma.com/blog/figma-on-figma-how-we-built-figma-dot-coms-design-system/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FiFigma size={35} className="skills__icon" alt="Link to Figma" />
+            </a>
+          </li>
+          <li className="skills__item">
+            <div>CSS</div>
+            <a
+              href="https://www.w3schools.com/css/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaCss3Alt size={35} className="skills__icon" alt="Link to CSS" />
+            </a>
+          </li>
+          <li className="skills__item">
+            <div>Tailwind</div>
+            <a
+              href="https://tailwindui.com/documentation"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <SiTailwindcss
+                size={35}
+                className="skills__icon"
+                alt="Link to Tailwind"
+              />
+            </a>
+          </li>
+          <li className="skills__item">
+            <div>Bootstrap</div>
+            <a
+              href="https://alaskan-odyssey.vercel.app/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <SiBootstrap
+                size={35}
+                className="skills__icon"
+                alt="Link to Bootstrap"
+              />
+            </a>
+          </li>
+          <li className="skills__item">
+            <div>MaterialUI</div>
+            <a
+              href="https://mytodolist-six.vercel.app/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg"
+                height="40px"
+              />
+            </a>
+          </li>
+          <li className="skills__item">
+            <div>NodeJs</div>
+            <a
+              href="https://nodejs.org/en/docs/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <GrNode size={35} className="skills__icon" alt="Link to Node" />
+            </a>
+          </li>
+          <li className="skills__item">
+            <div>Express</div>
+            <a
+              href="https://expressjs.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <SiExpress
+                size={35}
+                className="skills__icon"
+                alt="Link to Express"
+              />
+            </a>
+          </li>
+          <li className="skills__item">
+            <div>Sequelize</div>
+            <a
+              href="https://sequelize.org/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <SiSequelize
+                size={35}
+                className="skills__icon"
+                alt="Link to Sequelize"
+              />
+            </a>
+          </li>
+          <li className="skills__item">
+            <div>Git</div>
+            <a
+              href="https://git-scm.com/doc"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaGitAlt size={35} className="skills__icon" alt="Link to Git" />
+            </a>
+          </li>
+          {/* <li className="skills__item">
               <div>Scrum</div>
               <a
                 href="https://www.scrum.org/forum/scrum-forum/14437/agile-and-documentation"
@@ -242,8 +235,9 @@ const About = () => {
                 <DiScrum size={35} className="skills__icon" alt="Link to Scrum" />
               </a>
             </li> */}
-          </ul>
-        </div>
+        </ul>
+
+        {/* skills end ---------------------  */}
       </div>
     </div>
   );
