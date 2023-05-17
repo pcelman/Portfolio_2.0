932fb87147b1964f5428fbb6b48af687ee1c0f73
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2023-05-17 00:32:48 -0300

    fix: card size

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,19 +13,16 @@ export default function Projects() {
   return (
     <div className="projects" id="projects">
       <div className="projects__container">
-        {/* <h2 className="container__title">Projects</h2> */}
-        {/* <span className="line"></span> */}
-        <div className="content">
           <section className="projects__card">
-            <img className="project__image"
+            <img className="projects__card__image"
               src={projects__hc}
               alt="image of the design for Henry connect on figma"
               width="300px"
               height="250px"
             />
             <div>
-              <h2 className="projects__title" >HENRYconnect</h2>
-              <p className="projects__paragraph">
+              <h2 className="projects__card__title" >HENRYconnect</h2>
+              <p className="projects__card__paragraph">
                 I was part of a team that developed Henry Connect, a
                 location-based chat application built with React Native. My role
                 focused on the front-end development and design, including
@@ -35,17 +32,19 @@ export default function Projects() {
               <a
                 href="https://www.figma.com/file/2NzlH6zSqLfxevWvKNVJCU/P-connect?node-id=0%3A1&t=tCm1GvsENAYSyUCP-1"
                 target="_blank"
-                className="projects__icon-center"
+                className="projects__card__icon-center"
               >
-                <FiFigma size={30} className="projects__icon" alt="Link to Figma"/>
+                <FiFigma size={30} className="projects__card__icon" alt="Link to Figma"/>
               </a>
             </div>
           </section>
           <section className="projects__card">
-            <img className="projects__image" src={projects__arq} alt="Arquihub" width="300px" height="250px" />
+            <img className="projects__card__image" src={projects__arq} alt="Arquihub" 
+            width="300px" height="250px" 
+            />
             <div>
-              <h2 className="projects__title">ARQUIHUB</h2>
-              <p className="projects__paragraph">
+              <h2 className="projects__card__title">ARQUIHUB</h2>
+              <p className="projects__card__paragraph">
                 As a member of the front-end team, I contributed to the
                 development of Arquihub. My responsibilities included creating
                 and implementing various React components, contributing to the
@@ -53,25 +52,27 @@ export default function Projects() {
                 functionalities.
               </p>
               <br />
-              <div className="projects__icon-center">
+              <div className="projects__card__icon-center">
                 <a href="https://github.com/pcelman/ARQUIHUB" target="_blank">
                   <GoMarkGithub
                     size={30}
-                    className="projects__icon-center-github"
+                    className="projects__card__icon-center-github"
                     alt="Link to Github"
                   />
                 </a>
                 <a href="https://arquihub.vercel.app/" target="_blank">
-                  <BiWorld size={30} className="projects__icon-center-world" alt="Link to the deploy of Arquihub"/>
+                  <BiWorld size={30} className="projects__card__icon-center-world" alt="Link to the deploy of Arquihub"/>
                 </a>
               </div>
             </div>
           </section>
           <section className="projects__card">
-            <img className="projects__image" src={projects__fm} alt="Fundacion Mujer project" width="300px" height="250px" />
+            <img className="projects__card__image" src={projects__fm} alt="Fundacion Mujer project" 
+            width="300px" height="250px" 
+            />
             <div>
-              <h2 className="projects__title">Fundacion Mujer</h2>
-              <p className="projects__paragraph">
+              <h2 className="projects__card__title">Fundacion Mujer</h2>
+              <p className="projects__card__paragraph">
                 I was part of the front-end team for a foundation based in
                 Colombia. The goal of the project was to create a website that
                 provided a platform for women to access news, posts, and job
@@ -81,17 +82,19 @@ export default function Projects() {
               <a
                 href="https://www.figma.com/file/4R1YzisyHOhWE0j228Qk2V/P-Fundacion?t=tCm1GvsENAYSyUCP-1"
                 target="_blank"
-                className="projects__icon-center"
+                className="projects__card__icon-center"
               >
-                <FiFigma size={30} className="projects__icon" alt="Link to Figma"/>
+                <FiFigma size={30} className="projects__card__icon" alt="Link to Figma"/>
               </a>
             </div>
           </section>
           <section className="projects__card">
-            <img className="projects__image" src={projects__vg} alt="Videogames project" width="300px" height="250px" />
+            <img className="projects__card__image" src={projects__vg} alt="Videogames project" 
+            width="300px" height="250px" 
+            />
             <div>
-              <h2 className="projects__title">Videogames</h2>
-              <p className="projects__paragraph">
+              <h2 className="projects__card__title">Videogames</h2>
+              <p className="projects__card__paragraph">
                 I was the sole developer for a personal project creating a video
                 game website. My responsibilities included all aspects of the
                 project, from API requests to the final rendering and
@@ -103,17 +106,19 @@ export default function Projects() {
               <a
                 href="https://github.com/pcelman/Videogames"
                 target="_blank"
-                className="projects__icon-center"
+                className="projects__card__icon-center"
               >
-                <GoMarkGithub size={30} className="projects__icon" alt="Link to Github"/>
+                <GoMarkGithub size={30} className="projects__card__icon" alt="Link to Github"/>
               </a>
             </div>
           </section>
           <section className="projects__card">
-            <img className="projects__image" src={projects__pk} alt="Pokemon project" width="300px" height="250px" />
+            <img className="projects__card__image" src={projects__pk} alt="Pokemon project" 
+            width="300px" height="250px" 
+            />
             <div>
-              <h2 className="projects__title">Pokemon</h2>
-              <p  className="projects__paragraph">
+              <h2 className="projects__card__title">Pokemon</h2>
+              <p  className="projects__card__paragraph">
                 I worked independently on a personal project creating a website
                 dedicated to Pokemon. The project involved using Express for
                 routing, Sequelize and Postgres for the database, and React and
@@ -124,15 +129,15 @@ export default function Projects() {
               <a
                 href="https://github.com/pcelman/Make-your-own"
                 target="_blank"
-                className="projects__icon-center"
+                className="projects__card__icon-center"
               >
-                <GoMarkGithub size={30} className="projects__icon" alt="Link to Github"/>
+                <GoMarkGithub size={30} className="projects__card__icon" alt="Link to Github"/>
               </a>
             </div>
           </section>
       
         </div>
       </div>
-    </div>
+    // </div>
   );
 }
