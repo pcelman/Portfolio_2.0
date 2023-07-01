a8aa7d914ffea61403bf83fa2acb8dc53d4b6e1e
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2023-06-30 21:34:15 -0300

    update: links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,6 +13,121 @@ export default function Projects() {
   return (
     <div className="projects" id="projects">
       <div className="projects__container">
+
+        <section className="projects__card">
+          <img
+            className="projects__card__image"
+            src={projects__arq}
+            alt="Arquihub"
+            width="300px"
+            height="250px"
+          />
+          <div>
+            <h2 className="projects__card__title">ARQUIHUB</h2>
+            <p className="projects__card__paragraph">
+              As a member of the front-end team, I contributed to the
+              development of Arquihub. My responsibilities included creating and
+              implementing various React components, contributing to the overall
+              design and aesthetics of the application, and adding
+              functionalities.
+            </p>
+            <br />
+            <br />
+            <div className="projects__card__icon-center">
+              <a href="https://arquihub.vercel.app/" target="_blank">
+                <BiWorld
+                  size={33}
+                  className="projects__card__icon-center-world"
+                  alt="Link to the deploy of Arquihub"
+                />
+              </a>
+              <a href="https://github.com/pcelman/ARQUIHUB" target="_blank">
+                <GoMarkGithub
+                  size={30}
+                  className="projects__card__icon-center-github"
+                  alt="Link to Github"
+                />
+              </a>
+            </div>
+          </div>
+        </section>
+        <section className="projects__card">
+          <img
+            className="projects__card__image"
+            src={projects__pk}
+            alt="Pokemon project"
+            width="300px"
+            height="250px"
+          />
+          <div>
+            <h2 className="projects__card__title">Pokemon</h2>
+            <p className="projects__card__paragraph">
+              Worked independently creating a website dedicated to Pokemon. The
+              project involved using Express for routing, Sequelize and Postgres
+              for the database, and React and Redux for the front-end. I
+              utilized an API to gather information and display it on the
+              website.
+            </p>
+            <br /> <br />
+            <a
+              href="https://github.com/pcelman/Pokemon-front.git"
+              target="_blank"
+              className="projects__card__icon-center"
+            >
+              <a href="https://my-pokemon-delta.vercel.app/" target="_blank">
+                <BiWorld
+                  size={33}
+                  className="projects__card__icon-center-world"
+                  alt="Link to the deploy of My Videogame"
+                />
+              </a>
+              <GoMarkGithub
+                size={30}
+                className="projects__card__icon"
+                alt="Link to Github"
+              />
+            </a>
+          </div>
+        </section>
+        
+        <section className="projects__card">
+          <img
+            className="projects__card__image"
+            src={projects__vg}
+            alt="Videogames project"
+            width="300px"
+            height="250px"
+          />
+          <div>
+            <h2 className="projects__card__title">Videogames</h2>
+            <p className="projects__card__paragraph">
+              Personal project. My responsibilities included all aspects of the
+              project, from API requests to the rendering and functionality of
+              the website. I utilized Express for routing, Sequelize and
+              Postgres for the database, and React and Redux for the front-end.
+              I sourced the information for the website from an API.
+            </p>
+            <a
+              href="https://github.com/pcelman/Videogames-front"
+              target="_blank"
+              className="projects__card__icon-center"
+            >
+              <a href="https://my-videogame.vercel.app/" target="_blank">
+                <BiWorld
+                  size={33}
+                  className="projects__card__icon-center-world"
+                  alt="Link to the deploy of My Videogame"
+                />
+              </a>
+              <GoMarkGithub
+                size={30}
+                className="projects__card__icon"
+                alt="Link to Github"
+              />
+            </a>
+          </div>
+        </section>
+ 
         <section className="projects__card">
           <img
             className="projects__card__image"
@@ -55,151 +170,6 @@ export default function Projects() {
                 />
               </a>
             </div>
-          </div>
-        </section>
-        <section className="projects__card">
-          <img
-            className="projects__card__image"
-            src={projects__arq}
-            alt="Arquihub"
-            width="300px"
-            height="250px"
-          />
-          <div>
-            <h2 className="projects__card__title">ARQUIHUB</h2>
-            <p className="projects__card__paragraph">
-              As a member of the front-end team, I contributed to the
-              development of Arquihub. My responsibilities included creating and
-              implementing various React components, contributing to the overall
-              design and aesthetics of the application, and adding
-              functionalities.
-            </p>
-            <br />
-            <br />
-            <div className="projects__card__icon-center">
-            <a href="https://arquihub.vercel.app/" target="_blank">
-                <BiWorld
-                  size={30}
-                  className="projects__card__icon-center-world"
-                  alt="Link to the deploy of Arquihub"
-                />
-              </a>
-              <a href="https://github.com/pcelman/ARQUIHUB" target="_blank">
-                <GoMarkGithub
-                  size={30}
-                  className="projects__card__icon-center-github"
-                  alt="Link to Github"
-                />
-              </a>
-         
-            </div>
-          </div>
-        </section>
-        <section className="projects__card">
-          <img
-            className="projects__card__image"
-            src={projects__vg}
-            alt="Videogames project"
-            width="300px"
-            height="250px"
-          />
-          <div>
-            <h2 className="projects__card__title">Videogames</h2>
-            <p className="projects__card__paragraph">
-              Personal project. My responsibilities included all aspects of the
-              project, from API requests to the rendering and
-              functionality of the website. I utilized Express for routing,
-              Sequelize and Postgres for the database, and React and Redux for
-              the front-end. I sourced the information for the website from an
-              API.
-            </p>
-            <a
-              href="https://github.com/pcelman/Videogames-front"
-              target="_blank"
-              className="projects__card__icon-center"
-            >
-                     <a href="https://my-videogame.vercel.app/" target="_blank">
-                <BiWorld
-                  size={30}
-                  className="projects__card__icon-center-world"
-                  alt="Link to the deploy of My Videogame"
-                />
-              </a>
-              <GoMarkGithub
-                size={30}
-                className="projects__card__icon"
-                alt="Link to Github"
-              />
-            </a>
-
-          </div>
-        </section>
-        <section className="projects__card">
-          <img
-            className="projects__card__image"
-            src={projects__fm}
-            alt="Fundacion Mujer project"
-            height="250px"
-             width="300px"
-          />
-          <div>
-            <h2 className="projects__card__title">Fundacion Mujer</h2>
-            <p className="projects__card__paragraph">
-              Part of the front-end team for a foundation based in
-              Colombia. The goal of the project was to create a website that
-              provided a platform for women to access news, posts, and job
-              opportunities. 
-            </p>
-            <br/>
-            <br/>
-            <br />
-            <br/>
-            <br/>
-
-            <a
-              href="https://www.figma.com/file/4R1YzisyHOhWE0j228Qk2V/P-Fundacion?t=tCm1GvsENAYSyUCP-1"
-              target="_blank"
-              className="projects__card__icon-center"
-            >
-              <FiFigma
-                size={30}
-                className="projects__card__icon"
-                alt="Link to Figma"
-              />
-            </a>
-          </div>
-        </section>
-    
-        <section className="projects__card">
-          <img
-            className="projects__card__image"
-            src={projects__pk}
-            alt="Pokemon project"
-            width="300px"
-            height="250px"
-          />
-          <div>
-            <h2 className="projects__card__title">Pokemon</h2>
-            <p className="projects__card__paragraph">
-              Worked independently creating a website
-              dedicated to Pokemon. The project involved using Express for
-              routing, Sequelize and Postgres for the database, and React and
-              Redux for the front-end. I utilized an API to gather information
-              and display it on the website.
-              
-            </p>
-            <br /> <br />
-            <a
-              href="https://github.com/pcelman/Make-your-own"
-              target="_blank"
-              className="projects__card__icon-center"
-            >
-              <GoMarkGithub
-                size={30}
-                className="projects__card__icon"
-                alt="Link to Github"
-              />
-            </a>
           </div>
         </section>
       </div>
